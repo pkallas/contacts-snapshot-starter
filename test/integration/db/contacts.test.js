47e7ea0aa0b8636e502a2b6c32548668f20f893e
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-10-11 11:25:01 -0700

    Added tests for findAll function

--- test/integration/db/contacts.test.js
+++ test/integration/db/contacts.test.js
@@ -1,4 +1,7 @@
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const db = require('../../helpers/db');
 const contacts = require('../../../src/models/contacts');
 
@@ -22,9 +25,30 @@ context('Contacts Database Functions', function () {
     });
 
     it('Should throw an error when input is not an object', function () {
-      return contacts.create('Patrick Kallas')
-      .catch(error => {
-        expect(error.message).to.eql('null value in column "first_name" violates not-null constraint');
+      return expect(contacts.create('Patrick Kallas')).to.eventually.be.rejected;
+    });
+  });
+
+  describe('findAll', function () {
+
+    it('Should return an empty array when the database has no contacts', function () {
+      return contacts.findAll()
+      .then(allContacts => {
+        expect(allContacts).to.eql([]);
+      });
+    });
+
+    it('Should return all contacts from the database', function () {
+      return db.seedContacts()
+      .then(() => {
+        return contacts.findAll()
+        .then(allContacts => {
+          expect(allContacts).eql([
+            { id: 1, first_name: 'Jared', last_name: 'Grippe' },
+            { id: 2, first_name: 'Tanner', last_name: 'Welsh' },
+            { id: 3, first_name: 'NeEddra', last_name: 'James' },
+          ]);
+        });
       });
     });
   });
